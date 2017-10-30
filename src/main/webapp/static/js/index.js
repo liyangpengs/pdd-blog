ee262779263f2d32f607d6718e16e78edb176785
'liyangpengs' <'871080854@qq.com'>
2017-10-30 15:33:39 +0800

    .

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,98 +1,26 @@
-// ----------------------------------------
-		// Particle
-		// ----------------------------------------
-		function Particle(x, y, radius) {
-			this.init(x, y, radius);
-		}
-		Particle.prototype = {
-			init : function(x, y, radius) {
-				this.alive = true;
-				this.radius = radius || 10;
-				this.wander = 0.15;
-				this.theta = random(TWO_PI);
-				this.drag = 0.92;
-				this.color = '#fff';
-				this.x = x || 0.0;
-				this.y = y || 0.0;
-				this.vx = 0.0;
-				this.vy = 0.0;
-			},
-			move : function() {
-				this.x += this.vx;
-				this.y += this.vy;
-				this.vx *= this.drag;
-				this.vy *= this.drag;
-				this.theta += random(-0.5, 0.5) * this.wander;
-				this.vx += sin(this.theta) * 0.1;
-				this.vy += cos(this.theta) * 0.1;
-				this.radius *= 0.96;
-				this.alive = this.radius > 0.5;
-			},
-			draw : function(ctx) {
-				ctx.beginPath();
-				ctx.arc(this.x, this.y, this.radius, 0, TWO_PI);
-				ctx.fillStyle = this.color;
-				ctx.fill();
-			}
-		};
-		// ----------------------------------------
-		// Example
-		// ----------------------------------------
-		var MAX_PARTICLES = 280;
-		var COLOURS = [ '#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900',
-				'#FF4E50', '#F9D423' ];
-		var particles = [];
-		var pool = [];
-		var demo = Sketch.create({
-			container : document.getElementById('test')
-		});
-		demo.setup = function() {
-			// Set off some initial particles.
-			var i, x, y;
-			//  for ( i = 0; i < 20; i++ ) {
-			x = (demo.width * 0.5) + random(-100, 100);
-			y = (demo.height * 0.5) + random(-100, 100);
-			demo.spawn(0, 999);
-			// }
-		};
-		demo.spawn = function(x, y) {
-			if (particles.length >= MAX_PARTICLES)
-				pool.push(particles.shift());
-			particle = pool.length ? pool.pop() : new Particle();
-			particle.init(x, y, random(5, 40));
-			particle.wander = random(0.5, 2.0);
-			particle.color = random(COLOURS);
-			particle.drag = random(0.9, 0.99);
-			theta = random(TWO_PI);
-			force = random(2, 8);
-			particle.vx = sin(theta) * force;
-			particle.vy = cos(theta) * force;
-			particles.push(particle);
-		};
-		demo.update = function() {
-			var i, particle;
-			for (i = particles.length - 1; i >= 0; i--) {
-				particle = particles[i];
-				if (particle.alive)
-					particle.move();
-				else
-					pool.push(particles.splice(i, 1)[0]);
-			}
-		};
-		demo.draw = function() {
-			demo.globalCompositeOperation = 'lighter';
-			for ( var i = particles.length - 1; i >= 0; i--) {
-				particles[i].draw(demo);
-			}
-		};
-		demo.mousemove = function() {
-			var particle, theta, force, touch, max, i, j, n;
-			for (i = 0, n = demo.touches.length; i < n; i++) {
-				touch = demo.touches[i], max = random(1, 4);
-				for (j = 0; j < max; j++) {
-					demo.spawn(touch.x, touch.y);
-				}
-			}
-		};
-		
-		
+//列表
+function foreachList(json){
+	for (var i = 0; i < json.length; i++) {
+		var html="<article class=\"excerpt excerpt-1\"><a class=\"focus\" href='"+json[i].url+"' draggable=\"false\">"+
+			"<img class=\"thumb\" data-original=\"\" src='"+json[i].imgUrl+"' alt=\"\" draggable=\"false\"></a>"+
+			"<header><a class=\"cat\" href='"+json[i].url+"' draggable=\"false\">"+json[i].news_type.tname+"<i></i></a>"+
+			"<h2><a href='"+json[i].url+"' title=\"\" draggable=\"false\">"+json[i].title+"</a></h2>"+
+			"</header><p class=\"meta\"><time class=\"time\"><i class=\"glyphicon glyphicon-time\"></i>"+json[i].publishtime+"</time>"+
+			"<span class=\"views\"><i class=\"glyphicon glyphicon-eye-open\"></i>共"+json[i].seecount+"人查看</span>"+
+			"<a class=\"comment\" href=\"http://www.ice-breaker.cn/article.html#comment\" draggable=\"false\"></a></p>"+
+			"<p class=\"note\">"+json[i].descs+"</p></article>";
+		//为内容添加元素
+		$("div.content").append(html);
+	}	
+}
+//热门
+function foreachHot(json){
+	for (var i = 0; i < json.length; i++) {
+		var html="<li><a href=\""+json[i].url+"\" draggable=\"false\">"+
+		"<span class=\"thumbnail\"><img class=\"thumb\" src=\""+json[i].imgUrl+"\" alt=\"\" draggable=\"false\"></span>"+
+		"<span class=\"text\">"+json[i].title+"</span>"+
+		"<span class=\"muted\"><i class=\"glyphicon glyphicon-time\"></i>"+json[i].publishtime+"</span>"+
+		"<span class=\"muted\"><i class=\"glyphicon glyphicon-eye-open\"></i>"+json[i].seecount+"</span></a></li>";
+		$("#hot").append(html);
+	}
+}
